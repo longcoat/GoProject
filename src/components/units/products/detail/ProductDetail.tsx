4252817e2032e620d530201d015b4bcda7fdec7b
longcoat <sherlockpkm@gmail.com>
2023-02-15 14:33:48 +0900

    20230215 회원가입 로그인 등록 레이아웃

---
+++ src/components/units/products/detail/ProductDetail.tsx
@@ -0,0 +1,10 @@
+import * as S from "./ProductDetail.styles";
+
+export default function ProductDetail() {
+  return (
+    <>
+      <S.Detail>디테일디테일디테일</S.Detail>
+      <div>디테일디테일</div>
+    </>
+  );
+}
